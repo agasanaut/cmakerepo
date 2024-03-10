9495b287b5335ec0c309b02d54de2aee088db18d
Tejus Satish <agasanaut@gmail.com>
2024-03-10 13:18:00 +0530

    integrate cmkae lib

--- rectangle_project/src/rectangle.cpp
+++ rectangle_project/src/rectangle.cpp
@@ -1,5 +1,5 @@
 #include "rectangle.h"
-
+#include "../../external/addition.h"
 Rectangle::Rectangle(double w, double h) {
     width = w;
     height = h;
@@ -24,6 +24,9 @@ int main() {
 
     // Calculate and print the perimeter of the rectangle
     std::cout << "Perimeter of the rectangle: " << rectangle.perimeter() << std::endl;
+
+    // Calc output of lib
+    std::cout << "lib out : " << Addition::add(4,5) << std::endl;
 
     return 0;
 }
